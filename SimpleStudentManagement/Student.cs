3299bcebf0df445c516c6e6ad124e2d2a3118729
Alessandro Volcich <volcich.alessandro@hotmail.com>
2017-10-19 19:29:32 +0200

    Add project files.

---
+++ SimpleStudentManagement/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStudentManagement
+{
+    class Student
+    {
+        public string vorname { get; set; }
+        public string nachname { get; set; }
+        public int alter { get; set; }
+        public string studiengang { get; set; }
+        public Boolean beitragBezahlt { get; set; }
+    }
+}
